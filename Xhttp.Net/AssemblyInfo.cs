78ea3cb4726f84327546019527306af52a047153
Juan Benavides Romero <jbalde@gmail.com>
2012-05-12 13:51:50 +0200

    Base project with readme and license

---
+++ Xhttp.Net/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Xhttp.Net")]
+[assembly: AssemblyDescription("The Xhttp.Net framework is a dotnet implementation of the Extended Hypertext Transfer Protocol (http://www.xhttp.org), with simple service integration, full arguments support including Base64 and DateTime, single and multiple asynchronous requests, data streaming, remote API creation from XHTTP service schemas, and a runtime plugin architecture.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Altair Studios")]
+[assembly: AssemblyProduct("Xhttp.Net")]
+[assembly: AssemblyCopyright("Altair Studios (Juan Benavides Romero)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
